74a2b8cdcec59b313fa3a37a41ee67a5ee938b57
kumarikiran <kiranashakumari@gmail.com>
2025-01-28 22:53:59 +0100

    renamed

--- plot_heatmap.py
+++ plot_heatmap.py
@@ -1,42 +1,94 @@
 
+
 import numpy as np
 import pandas as pd 
 import seaborn as sns 
 import random
 import matplotlib.pyplot as plt
-from scipy.optimize import curve_fit
-# from function_contact import contact_ps
-# from function_contact import contact_ps_start
+from matplotlib.colors import LogNorm, Normalize
 
+### parameters 
+v=20
+rc=0.1
 L = 1000
-nu = -0.7
-rc = np.array((10, 5, 1))  
-v = np.array((0.1,0.25, 0.5, 1))  
-fig, axes = plt.subplots(v.shape[0], rc.shape[0], 
-                         figsize=(rc.shape[0]*5, v.shape[0]*5), 
-                         sharex=True, 
-                         sharey=True)
 
+rc = np.array((0.2, 0.8))
+rc_value = 5
+v = np.array((0.1,0.25, 1.0))
+# v = np.array((0.1,1, 10))
+# v = v *L
 
+# Create subplots with 4 columns and 1 row
+fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(15, 5))  # Adjust figsize as needed
+# Adjust spacing between columns (optional)
+plt.subplots_adjust(wspace=0.4) 
+x_limits = (0, 1000)
+y_limits = (0, 1000)
 for v_i, v_value  in enumerate(v): # parameter loop
-    for rc_i, rc_value in enumerate(rc): # parameter loop 
+    # for rc_i, rc_value in enumerate(rc): # parameter loop 
         cont = np.zeros((L,L))
-        data =  pd.read_csv('cp_data_v%.6f_rc%.6f.csv' %(v_value, rc_value))
+        data =  pd.read_csv('data_cp/data_orc_mid/cp_data_v%.6f_rc%.6f.csv' %(v_value, rc_value), index_col=0)   
         
-        ax=axes[v_i, rc_i]
-        ax.set_title(f'v={v_value}, rc={rc_value}(s)^({nu})', fontsize = 24)  
-        sns.heatmap(data, ax=axes[v_i, rc_i], cmap="Reds",cbar=True,
-                    vmin= 0, vmax=1000)
+        ### make a reference matrix 
+        nu = 0.7
+        import random 
+        data_ref = np.zeros((1000, 1000), dtype = float)
+        for i in range (0, 1000):
+            for j in range (i+1, 1000):
+                # random_number = random.random()
+                # rc_rule = rc_value* abs(i -j)**(nu)
+                # if random_number < rc_rule:
+                data_ref[i,j] = data_ref[i,j] +abs(i -j)**(nu)
+                data_ref[j,i] = data_ref[i,j]
+        data_ref = data_ref/data_ref.max().max() 
+                    
+                    
+        ax=axes[v_i]
+        color_map = plt.cm.get_cmap('Reds')
+        heatmap = ax.imshow(data, cmap=color_map, interpolation='nearest',norm=LogNorm(vmin=1, vmax=500))
         
-        # Draw the heatmap
-        # sns.heatmap(data, ax=ax, cmap="Reds", cbar=v_i == len(v_value) - 1 and rc_i == len(rc_value) - 1,
-        #             cbar_ax=None if not (v_i == len(v_value) - 1 and rc_i == len(rc_value) - 1) else cbar_ax,
-        #             vmin=0.0001, vmax=0.1)
+        cbar = fig.colorbar(heatmap, ax=ax, fraction=0.046, pad=0.04)
+        cbar.ax.tick_params(labelsize=14)
+        # v_value = int(v_value)
+        ax.set_title(f'$v={v_value}$', fontsize = 24)    
+        ax.tick_params(axis='x', labelsize=24) 
         
-        ax.tick_params(axis='both', which='major', labelsize=18)  # Adjust tick label fontsize
-        ax.set_xlabel(ax.get_xlabel(), fontsize=20)  # Adjust x-axis label fontsize
-        ax.set_ylabel(ax.get_ylabel(), fontsize=20)                    
+        ############## for the ps vs  s
+        # y_limits = (0.8, 5000)
+        # x_limits = (8, 1000)
+        # ps = np.zeros(L)
+        # for i in range (1,L):
+        #     ps[i-1] = np.diag(data,i).mean()
+        # ax.plot(ps,'c--', marker='o', linewidth= 1)    
+        # ax.set_xscale('log')
+        # ax.set_yscale('log')
+for ax in axes:
+    ax.tick_params(axis='y', labelsize=16)
+    ax.tick_params(axis='x', labelsize=16)
+    ax.spines['top'].set_linewidth(1)
+    ax.spines['bottom'].set_linewidth(1)
+    ax.spines['left'].set_linewidth(1)
+    ax.spines['right'].set_linewidth(1)
+    ax.set_xlim(x_limits)
+    ax.set_ylim(y_limits)
+    # ax.set_xlabel('s', fontsize=24)
+    # ax.set_ylabel('P(s)', fontsize=24)
+
+        
+plt.subplots_adjust(wspace=0.4)            
+        
+# sns.heatmap(cont/cont.max().max())
+                
    # Adjust layout to prevent clipping of titles
 plt.tight_layout()
-plt.savefig('cont_L%d_nu%.2f_1.png' %(L, nu))
-plt.show()      
+plt.savefig('aa_plot.pdf')
+
+# plt.savefig('uniform_500.png')
+plt.show()             
+
+
+
+
+            
+        
+    
